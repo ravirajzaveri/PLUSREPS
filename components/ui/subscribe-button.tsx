d462d70283c8f2ff4b4369cb08c434051e4eb149
ravirajzaveri <38056381+ravirajzaveri@users.noreply.github.com>
2025-05-18 19:58:43 +0530

    Create subscribe-button.tsx

---
+++ components/ui/subscribe-button.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+
+export const SubscribeButton = ({ userId }: { userId: string }) => {
+  return (
+    <Button variant="primary">
+      Subscribe
+    </Button>
+  );
+};
