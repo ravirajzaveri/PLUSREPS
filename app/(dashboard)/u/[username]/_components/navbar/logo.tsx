f1d372ab66c15d64231ba3b1e874b97f27543fff
ravirajzaveri <38056381+ravirajzaveri@users.noreply.github.com>
2025-05-18 11:52:54 +0530

    Update logo.tsx

--- app/(dashboard)/u/[username]/_components/navbar/logo.tsx
+++ app/(dashboard)/u/[username]/_components/navbar/logo.tsx
@@ -14,10 +14,10 @@ export const Logo = () => {
     <Link href="/">
       <div className="flex items-center gap-x-4 hover:opacity-75 transition">
         <div className="bg-white rounded-full p-3 mr-12 shrink-0 lg:mr-0 lg:shrink">
-          <Image src="/twitch.svg" alt="Logo" width={32} height={32} />
+          <Image src="/fitstreamlogo.svg" alt="Logo" width={32} height={32} />
         </div>
         <div className={cn("hidden lg:block", font.className)}>
-          <p className="text-lg font-semibold">Twitch</p>
+          <p className="text-lg font-semibold">fitstream</p>
         </div>
       </div>
     </Link>
