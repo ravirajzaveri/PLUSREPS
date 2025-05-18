490c2d0dd4db5079cdd6acf192d92e49dbdb5a4b
ravirajzaveri <38056381+ravirajzaveri@users.noreply.github.com>
2025-05-18 13:14:56 +0530

    Update live-video.tsx

--- components/stream-player/live-video.tsx
+++ components/stream-player/live-video.tsx
@@ -87,22 +87,3 @@ export const LiveVideo = ({ participant }: LiveVideoProps) => {
     );
   }
 
-  return (
-    <div ref={wrapperRef} className="relative h-full flex">
-      <video ref={videoRef} width="100%" />
-      <div className="absolute top-0 h-full w-full opacity-0 hover:opacity-100 hover:transition-all">
-        <div className="absolute bottom-0 flex h-14 w-full items-center justify-between bg-gradient-to-r from-neutral-900 px-4">
-          <VolumeControl
-            onChange={onVolumeChange}
-            value={volume}
-            onToggle={toggleMute}
-          />
-          <FullscreenControl
-            isFullscreen={isFullscreen}
-            onToggle={toggleFullscreen}
-          />
-        </div>
-      </div>
-    </div>
-  );
-};
