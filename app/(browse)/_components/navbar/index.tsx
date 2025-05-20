17e778e2bf7e3fa550fbd15a34d9f87006241e23
ravirajzaveri <38056381+ravirajzaveri@users.noreply.github.com>
2025-05-20 22:38:23 +0530

    Update index.tsx

--- app/(browse)/_components/navbar/index.tsx
+++ app/(browse)/_components/navbar/index.tsx
@@ -1,3 +1,6 @@
+// app/(browse)/_components/navbar/index.tsx
+
+import Link from "next/link";
 import { Logo } from "./logo";
 import { Search } from "./search";
 import { Actions } from "./actions";
@@ -6,6 +9,15 @@ export const Navbar = () => {
   return (
     <nav className="fixed top-0 left-0 w-full h-20 bg-[#252731] z-49 px-2 lg:px-4 flex items-center justify-between shadow-md">
       <Logo />
+
+      {/* Simple Legal link */}
+      <Link
+        href="/legal/page"
+        className="text-sm text-white hover:underline mr-4"
+      >
+        Legal & Help
+      </Link>
+
       <Search />
       <Actions />
     </nav>
