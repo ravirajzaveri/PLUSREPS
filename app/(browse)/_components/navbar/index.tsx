87bbc957ec902680e7ea8980c9168d24a0b632f1
ravirajzaveri <38056381+ravirajzaveri@users.noreply.github.com>
2025-05-20 23:50:14 +0530

    Update index.tsx

--- app/(browse)/_components/navbar/index.tsx
+++ app/(browse)/_components/navbar/index.tsx
@@ -8,26 +8,22 @@ import { Actions } from "./actions";
 export const Navbar = () => {
   return (
     <nav className="fixed top-0 left-0 w-full h-20 bg-[#252731] z-49 px-2 lg:px-4 flex items-center justify-between shadow-md">
-      {/* Left Section: Logo + Legal */}
+      {/* Left: Logo + Legal link stacked in flex row */}
       <div className="flex items-center space-x-4">
         <Logo />
         <Link
           href="/legal"
           className="text-sm text-white hover:underline"
         >
-          Legal & Help
+          Help
         </Link>
       </div>
 
-      {/* Center: Search - absolutely centered */}
-      <div className="absolute left-1/2 transform -translate-x-1/2 w-full max-w-md px-4">
-        <Search />
-      </div>
+      {/* Center: Search (remains responsive) */}
+      <Search />
 
       {/* Right: Actions */}
-      <div>
-        <Actions />
-      </div>
+      <Actions />
     </nav>
   );
 };
