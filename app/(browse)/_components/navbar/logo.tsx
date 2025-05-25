440b5406248b2832c000d6cb09bc4ebc682f227e
ravirajzaveri <38056381+ravirajzaveri@users.noreply.github.com>
2025-05-25 20:38:34 +0530

    Update logo.tsx

--- app/(browse)/_components/navbar/logo.tsx
+++ app/(browse)/_components/navbar/logo.tsx
@@ -14,10 +14,10 @@ export const Logo = () => {
     <Link href="/">
       <div className="flex items-center gap-x-4 hover:opacity-75 transition">
         <div className="bg-white rounded-full p-3 mr-12 shrink-0 lg:mr-0 lg:shrink">
-          <Image src="/fitstreamlogo.svg" alt="Logo" width={32} height={32} />
+          <Image src="/PLUSREPSLOGOPR.svg" alt="Logo" width={32} height={32} />
         </div>
         <div className={cn("hidden lg:block", font.className)}>
-          <p className="text-lg font-semibold">fitstream</p>
+          <p className="text-lg font-semibold">PLUSREPS</p>
         </div>
       </div>
     </Link>
