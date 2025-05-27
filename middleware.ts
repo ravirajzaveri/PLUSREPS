3f8e717bfd7891ce267a436b6115448b826dfadd
ravirajzaveri <38056381+ravirajzaveri@users.noreply.github.com>
2025-05-27 20:05:38 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -6,6 +6,7 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
   publicRoutes: [
     "/",
+    "/(browse)/(home)",       // ✅ explicitly allow your home tab
     "/api/webhooks(.*)",
     "/api/uploadthing",
     "/:username",
