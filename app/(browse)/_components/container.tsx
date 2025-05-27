1bf7dcca2cac48627090c43718d8e563ab7187d8
ravirajzaveri <38056381+ravirajzaveri@users.noreply.github.com>
2025-05-27 22:26:06 +0530

    Update container.tsx

--- app/(browse)/_components/container.tsx
+++ app/(browse)/_components/container.tsx
@@ -1,8 +1,7 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
-
 import { cn } from "@/lib/utils";
 import { useSidebar } from "@/store/use-sidebar";
 
@@ -12,7 +11,12 @@ interface ContainerProps {
 
 export const Container = ({ children }: ContainerProps) => {
   const matches = useMediaQuery("(max-width: 1024px)");
+  const [hydrated, setHydrated] = useState(false);
   const { collapsed, onCollapse, onExpand } = useSidebar((state) => state);
+
+  useEffect(() => {
+    setHydrated(true);
+  }, []);
 
   useEffect(() => {
     if (matches) {
@@ -22,15 +26,16 @@ export const Container = ({ children }: ContainerProps) => {
     }
   }, [matches, onCollapse, onExpand]);
 
-return (
-  <div
-    className={cn(
-      "flex-1",
-      matches ? "ml-0" : collapsed ? "ml-[70px]" : "ml-60"
-    )}
-  >
-    {children}
-  </div>
-);
+  if (!hydrated) return null;
 
+  return (
+    <div
+      className={cn(
+        "flex-1",
+        matches ? "ml-0" : collapsed ? "ml-[70px]" : "ml-60"
+      )}
+    >
+      {children}
+    </div>
+  );
 };
