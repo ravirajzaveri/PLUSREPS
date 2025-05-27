de5375df7fb9c4d86df85cbfae5ca6fa93486c18
ravirajzaveri <38056381+ravirajzaveri@users.noreply.github.com>
2025-05-27 18:02:39 +0530

    Update index.tsx
    changed livekit chat section for mobile compat

--- components/stream-player/index.tsx
+++ components/stream-player/index.tsx
@@ -59,11 +59,31 @@ export const StreamPlayer = ({
         token={token}
         serverUrl={process.env.NEXT_PUBLIC_LIVEKIT_WS_URL!}
         className={cn(
-          "grid grid-cols-1 lg:gap-y-0 lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 h-full",
+          "h-[100dvh] w-full", // mobile full height
+          "lg:grid lg:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-6 lg:gap-y-0",
           collapsed && "lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-2"
         )}
       >
-        <div className="space-y-4 col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 lg:overflow-y-auto hidden-scrollbar pb-10">
+        {/* ✅ Mobile layout */}
+        <div className="flex flex-col h-full lg:hidden">
+          <div className="shrink-0">
+            <Video hostName={user.username} hostIdentity={user.id} />
+          </div>
+          <div className="flex-1 overflow-hidden flex flex-col">
+            <Chat
+              viewerName={name}
+              hostName={user.username}
+              hostIdentity={user.id}
+              isFollowing={isFollowing}
+              isChatEnabled={stream.isChatEnabled}
+              isChatDelayed={stream.isChatDelayed}
+              isChatFollowersOnly={stream.isChatFollowersOnly}
+            />
+          </div>
+        </div>
+      
+        {/* ✅ Desktop layout */}
+        <div className="hidden lg:block col-span-1 lg:col-span-2 xl:col-span-2 2xl:col-span-5 overflow-y-auto hidden-scrollbar pb-10">
           <Video hostName={user.username} hostIdentity={user.id} />
           <Header
             hostName={user.username}
@@ -87,7 +107,8 @@ export const StreamPlayer = ({
             followedByCount={user._count.followedBy}
           />
         </div>
-        <div className={cn("col-span-1", collapsed && "hidden")}>
+      
+        <div className={cn("hidden lg:block col-span-1", collapsed && "hidden")}>
           <Chat
             viewerName={name}
             hostName={user.username}
@@ -99,6 +120,7 @@ export const StreamPlayer = ({
           />
         </div>
       </LiveKitRoom>
+
     </>
   );
 };
