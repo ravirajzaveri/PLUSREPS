d133c2529f2e4e2494136fb5a9b67f88302170a6
ravirajzaveri <38056381+ravirajzaveri@users.noreply.github.com>
2025-05-28 01:10:20 +0530

    Delete app/(browse)/(home)/_components/live-reels-view.tsx

--- app/(browse)/(home)/_components/live-reels-view.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-export const LiveReelsView = () => {
-  return (
-    <div className="flex items-center justify-center h-full text-muted-foreground">
-      Live stream reel view (coming next)
-    </div>
-  );
-};
