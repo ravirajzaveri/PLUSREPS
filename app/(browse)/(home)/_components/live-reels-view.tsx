114aad2ef8892d2424de08cd9ed6d915efe67b9b
ravirajzaveri <38056381+ravirajzaveri@users.noreply.github.com>
2025-05-27 19:34:12 +0530

    Create live-reels-view.tsx

---
+++ app/(browse)/(home)/_components/live-reels-view.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export const LiveReelsView = () => {
+  return (
+    <div className="flex items-center justify-center h-full text-muted-foreground">
+      Live stream reel view (coming next)
+    </div>
+  );
+};
