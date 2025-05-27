cb78501c8e1c40632858033aa1c6a9cc4021115a
ravirajzaveri <38056381+ravirajzaveri@users.noreply.github.com>
2025-05-27 16:56:41 +0530

    Update video.tsx
    down arrow to min stream and go home

--- components/stream-player/video.tsx
+++ components/stream-player/video.tsx
@@ -41,8 +41,18 @@ export const Video = ({ hostName, hostIdentity }: VideoProps) => {
 
 export const VideoSkeleton = () => {
   return (
-    <div className="aspect-video border-x border-background">
-      <Skeleton className="h-full w-full rounded-none" />
+    <div className="aspect-video border-b group relative">
+      {/* Down arrow visible on mobile */}
+      <button
+        onClick={() => window.location.href = "/"}
+        className="lg:hidden absolute top-2 left-2 z-50 bg-black bg-opacity-50 p-1 rounded-full"
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 9l-7 7-7-7" />
+        </svg>
+      </button>
+      {content}
     </div>
   );
+
 };
