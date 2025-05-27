61501d4d46a2cbbed284edf1bd86991d931391f5
ravirajzaveri <38056381+ravirajzaveri@users.noreply.github.com>
2025-05-27 19:21:27 +0530

    Update layout.tsx

--- app/(browse)/(home)/layout.tsx
+++ app/(browse)/(home)/layout.tsx
@@ -2,10 +2,22 @@ import { Navbar } from "../_components/navbar";
 
 const BrowseLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <>
-      <Navbar />
-      <div className="flex h-fll pt-20">{children}</div>
-    </>
+    <div className="flex flex-col h-[100dvh] overflow-hidden">
+      {/* Sticky Navbar */}
+      <div className="sticky top-0 z-50 bg-background">
+        <Navbar />
+      </div>
+
+      {/* Scrollable Content */}
+      <main className="flex-1 overflow-y-auto">
+        {children}
+      </main>
+
+      {/* Future: Sticky Tab Bar (optional) */}
+      {/* <div className="sticky bottom-0 z-50 bg-background border-t">
+        <MobileNavBar />
+      </div> */}
+    </div>
   );
 };
 
