1ad0d9c7d7d194c02e86876cd1d5859b85dd5b71
ravirajzaveri <38056381+ravirajzaveri@users.noreply.github.com>
2025-05-27 17:28:22 +0530

    Update layout.tsx

--- app/(browse)/layout.tsx
+++ app/(browse)/layout.tsx
@@ -23,24 +23,3 @@ const BrowseLayout = ({ children }: { children: React.ReactNode }) => {
 };
 
 export default BrowseLayout;
-import { Suspense } from "react";
-
-import { Navbar } from "./_components/navbar";
-import { Sidebar, SidebarSkeleton } from "./_components/sidebar";
-import { Container } from "./_components/container";
-
-const BrowseLayout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <>
-      <Navbar />
-      <div className="flex h-full pt-20">
-        <Suspense fallback={<SidebarSkeleton />}>
-          <Sidebar />
-        </Suspense>
-        <Container>{children}</Container>
-      </div>
-    </>
-  );
-};
-
-export default BrowseLayout;
