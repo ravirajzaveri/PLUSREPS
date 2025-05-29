a483390b21aaa3869f85b59fc1ca19b813e6c17a
ravirajzaveri <38056381+ravirajzaveri@users.noreply.github.com>
2025-05-29 20:54:15 +0530

    Update following-view.tsx

--- components/home/following-view.tsx
+++ components/home/following-view.tsx
@@ -1,19 +1,23 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { Skeleton } from "@/components/ui/skeleton";
 import { Follow, User } from "@prisma/client";
-import { Following, FollowingSkeleton } from "@/app/(browse)/_components/sidebar/following";
 
-interface StreamData extends Follow {
-  following: User & {
-    stream: { isLive: boolean } | null;
-  };
+import { useSidebar } from "@/store/use-sidebar";
+import { UserItem, UserItemSkeleton } from "@/app/(browse)/_components/sidebar/following/user-item";
+
+interface FollowingProps {
+  data: (Follow & {
+    following: User & {
+      stream: { isLive: boolean } | null;
+    };
+  })[];
 }
 
 export const FollowingView = () => {
-  const [data, setData] = useState<StreamData[]>([]);
+  const [data, setData] = useState<FollowingProps["data"]>([]);
   const [loading, setLoading] = useState(true);
+  const { collapsed } = useSidebar((state) => state);
 
   useEffect(() => {
     const fetchStreams = async () => {
@@ -31,7 +35,35 @@ export const FollowingView = () => {
     fetchStreams();
   }, []);
 
-  if (loading) return <FollowingSkeleton />;
+  if (loading) {
+    return (
+      <ul className="px-2 pt-2 lg:pt-0">
+        {[...Array(3)].map((_, i) => (
+          <UserItemSkeleton key={i} />
+        ))}
+      </ul>
+    );
+  }
 
-  return <Following data={data} />;
+  if (!data.length) return null;
+
+  return (
+    <div>
+      {!collapsed && (
+        <div className="pl-6 mb-4">
+          <p className="text-sm text-muted-foreground">Following</p>
+        </div>
+      )}
+      <ul className="space-y-2 px-2">
+        {data.map((follow) => (
+          <UserItem
+            key={follow.following.id}
+            username={follow.following.username}
+            imageUrl={follow.following.imageUrl}
+            isLive={follow.following.stream?.isLive}
+          />
+        ))}
+      </ul>
+    </div>
+  );
 };
