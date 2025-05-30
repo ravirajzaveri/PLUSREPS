0052a5ff4448088cfde6ee4a875614435dc5ebcd
ravirajzaveri <38056381+ravirajzaveri@users.noreply.github.com>
2025-05-31 00:54:01 +0530

    Update index.tsx z-50

--- app/(browse)/_components/navbar/index.tsx
+++ app/(browse)/_components/navbar/index.tsx
@@ -7,7 +7,7 @@ import { Actions } from "./actions";
 
 export const Navbar = () => {
   return (
-    <nav className="fixed top-0 left-0 w-full h-20 bg-[#252731] z-49 px-2 lg:px-4 flex items-center justify-between shadow-md">
+    <nav className="fixed top-0 left-0 w-full h-20 bg-[#252731] z-50 px-2 lg:px-4 flex items-center justify-between shadow-md">
       {/* Left: Logo + Legal link stacked in flex row */}
       <div className="flex items-center space-x-4">
         <Logo />
